eb25768703113b572644a67b46baafb81a81ad09
alabamustapha <alabamustapha@gmail.com>
2017-02-01 12:45:56 +0100

    view cleared

--- storage/framework/views/9e105a306fdc5a400ebfd6a96669949858bddd2b.php
+++
@@ -1,6 +0,0 @@
-<div class="footer text-center">
-    <div>
-        <strong>Copyright</strong> <a href="http://www.redehub.com.ng" target="_blank" title="Rede Hub">Redehubng</a> &copy; <?php echo e('2015 - ' . date('Y')); ?>
-
-    </div>
-</div>
