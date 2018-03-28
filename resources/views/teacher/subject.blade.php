b169693d4c97002596529bcfd66dfd24a98a5d3f
alabamustapha <alabamustapha@gmail.com>
2018-03-28 13:28:03 +0100

    color code

--- resources/views/teacher/subject.blade.php
+++ resources/views/teacher/subject.blade.php
@@ -319,7 +319,7 @@
              <div class="col-lg-12">
                  <div class="ibox float-e-margins">
                      <div class="ibox-title">
-                         <h5>Update students Second term result</h5>
+                         <h5 class="text-success">Update students Second term result</h5>
                          <div class="ibox-tools">
                              <a class="collapse-link">
                                  <i class="fa fa-chevron-up"></i>
